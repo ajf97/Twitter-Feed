c43dc3198a2bb127042352291babbf2775f00ae6
ajf97 <ajf97@correo.ugr.es>
2019-03-10 12:00:56 +0100

    Todo funcionando

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,33 @@
-# Following relationships
+# Crear usuarios de ejemplo
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobarrr",
+             password_confirmation: "foobarrr",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
+end
+
+# Microposts
+users = User.order(created_at: :asc).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+
+# Relaciones de ejemplo
 users = User.all
 user  = users.first
 following = users[2..50]
